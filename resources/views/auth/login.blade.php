d1c06c840e9f7dfbbe16ad5a5e6f85335a7d39a6
Xzhrael <bmahendraaaaa@gmail.com>
2025-11-06 21:18:48 +0700

    temp fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <base href="../../../" />
     <title>PastiFIX - Login</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="shortcut icon" href="{{ asset('assets/img/logo.png') }}" />
-    
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;500;700&display=swap" rel="stylesheet">
-    
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
     <link href="{{ asset('plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
-    
+
     <link rel="stylesheet" href="{{ asset('assets/css/auth-new.css') }}">
 </head>
-<body id="kt_body" class="auth-page-new"> <div class="auth-container">
-        
+
+<body id="kt_body" class="auth-page-new">
+    <div class="auth-container">
+
         <div class="auth-container-left">
             <a href="/" class="auth-logo">
                 <img src="{{ asset('assets/img/logo.png') }}" alt="PastiFIX Logo">
@@ -28,34 +31,34 @@
 
         <div class="auth-container-right">
             <div class="auth-card">
-                
+
                 <div class="auth-tabs">
                     <a href="{{ route('register') }}">Sign Up</a>
                     <a href="{{ route('login') }}" class="active">Sign In</a>
                 </div>
 
-                    @if (session('success'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('success') }}
-                        </div>
-                    @endif
+                @if (session('success'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('success') }}
+                    </div>
+                @endif
 
-                <form class="form w-100" novalidate="novalidate" id="login_form"
-                      action="{{ route('login.auth') }}" method="POST">
+                <form class="form w-100" novalidate="novalidate" id="login_form" action="{{ route('login.auth') }}"
+                    method="POST">
                     @csrf
 
                     <div class="form-group-minimal">
                         <label for="username">Username</label>
                         <input type="text" id="username" name="username" autocomplete="off"
-                               class="form-control-minimal" required />
+                            class="form-control-minimal" required />
                     </div>
 
                     <div class="form-group-minimal">
                         <label for="password">Password</label>
-                        <input class="form-control-minimal" type="password" id="password"
-                               name="password" autocomplete="off" required />
+                        <input class="form-control-minimal" type="password" id="password" name="password"
+                            autocomplete="off" required />
                     </div>
-                    
+
                     <div class="d-flex justify-content-between align-items-center mb-4">
                         <label class="form-check form-check-inline">
                             <input class="form-check-input" type="checkbox" name="remember_me" value="1" />
@@ -64,8 +67,8 @@
                         <a href="#" class="link-forgot">Forgot Password?</a>
                     </div>
                     <br>
-                    <div class="g-recaptcha mb-4" data-sitekey="{{ env('CAPTCHA_SITE_KEY') }}"
-                         data-action="LOGIN"></div>
+                    <div class="g-recaptcha mb-4" data-sitekey="{{ env('CAPTCHA_SITE_KEY') }}" data-action="LOGIN">
+                    </div>
                     <br>
                     <div class="d-grid mb-4">
                         <button type="submit" id="login_submit" class="btn btn-brand-auth">
@@ -85,15 +88,15 @@
                         <a href="#"><i class="bi bi-linkedin"></i></a>
                     </div>
                 </form>
-                </div>
+            </div>
         </div>
-        
+
     </div>
 
     <script src="https://www.google.com/recaptcha/api.js"></script>
     <script src="{{ asset('plugins/global/plugins.bundle.js') }}"></script>
     <script src="{{ asset('js/scripts.bundle.js') }}"></script>
-    
+
     <script>
         $(document).ready(function() {
             $('#login_submit').on('click', function(event) {
@@ -120,9 +123,10 @@
                                 text: response.pesan,
                             }).then(() => {
                                 // [FIX 3] Arahkan ke URL dinamis dari Controller
-                                document.location = response.redirect_url; 
+                                document.location = response.redirect_url;
                             });
-                        } else if (response.status == '2fa_required' && response['2fa_required']) {
+                        } else if (response.status == '2fa_required' && response[
+                            '2fa_required']) {
                             window.location.href = response.redirect_url;
                         } else {
                             Swal.fire({
@@ -137,8 +141,8 @@
                         let firstErrorMessage = '';
                         for (let key in errors) {
                             if (errors.hasOwnProperty(key)) {
-                                firstErrorMessage = errors[key][0]; 
-                                break; 
+                                firstErrorMessage = errors[key][0];
+                                break;
                             }
                         }
                         Swal.fire({
@@ -153,4 +157,5 @@
         });
     </script>
 </body>
+
 </html>
--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <base href="../../../" />
     <title>PastiFIX - Registrasi</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="shortcut icon" href="{{ asset('assets/img/logo.png') }}" />
-    
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;500;700&display=swap" rel="stylesheet">
-    
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
     <link href="{{ asset('plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
-    
+
     <link rel="stylesheet" href="{{ asset('assets/css/auth-new.css') }}">
 </head>
-<body id="kt_body" class="auth-page-new"> 
+
+<body id="kt_body" class="auth-page-new">
 
     <div class="auth-container">
-        
+
         <div class="auth-container-left">
             <a href="/" class="auth-logo">
                 <img src="{{ asset('assets/img/logo.png') }}" alt="PastiFIX Logo">
@@ -30,44 +32,44 @@
 
         <div class="auth-container-right">
             <div class="auth-card">
-                
+
                 <div class="auth-tabs">
                     <a href="{{ route('register') }}" class="active">Sign Up</a>
                     <a href="{{ route('login') }}">Sign In</a>
                 </div>
 
                 <form class="form w-100" novalidate="novalidate" id="register_form"
-                      action="{{ route('register.post') }}" method="POST">
+                    action="{{ route('register.post') }}" method="POST">
                     @csrf
 
                     <div class="form-group-minimal">
                         <label for="name">Nama Lengkap</label>
                         <input type="text" id="name" name="name" autocomplete="off"
-                               class="form-control-minimal" required />
+                            class="form-control-minimal" required />
                     </div>
 
                     <div class="form-group-minimal">
                         <label for="username">Username</label>
                         <input type="text" id="username" name="username" autocomplete="off"
-                               class="form-control-minimal" required />
+                            class="form-control-minimal" required />
                     </div>
 
                     <div class="form-group-minimal">
                         <label for="email">Email</label>
                         <input type="email" id="email" name="email" autocomplete="off"
-                               class="form-control-minimal" required />
+                            class="form-control-minimal" required />
                     </div>
 
                     <div class="form-group-minimal">
                         <label for="password">Password</label>
-                        <input class="form-control-minimal" type="password" id="password"
-                               name="password" autocomplete="off" required />
+                        <input class="form-control-minimal" type="password" id="password" name="password"
+                            autocomplete="off" required />
                     </div>
 
                     <div class="form-group-minimal">
                         <label for="password_confirmation">Confirm Password</label>
                         <input class="form-control-minimal" type="password" id="password_confirmation"
-                               name="password_confirmation" autocomplete="off" required />
+                            name="password_confirmation" autocomplete="off" required />
                     </div>
 
                     <div class="d-grid mb-4">
@@ -81,20 +83,20 @@
                         <a href="{{ route('login') }}" class="link-register">Sign In</a>
                     </div>
                 </form>
-                </div>
+            </div>
         </div>
     </div>
 
     <script src="{{ asset('plugins/global/plugins.bundle.js') }}"></script>
     <script src="{{ asset('js/scripts.bundle.js') }}"></script>
-    
+
     @if ($errors->any())
         <script>
             // Pastikan dokumen siap
             document.addEventListener('DOMContentLoaded', function() {
                 // Ambil error pertama
                 var firstError = @json($errors->all())[0];
-                
+
                 Swal.fire({
                     icon: 'error',
                     title: 'Registrasi Gagal!',
@@ -104,6 +106,7 @@
             });
         </script>
     @endif
-    
+
 </body>
+
 </html>
